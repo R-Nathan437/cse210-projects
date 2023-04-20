1b3ca8bb372c87eef9e35b9ed288a3539833c8f9
Nathan Reynolds <rey21031@byui.edu>
2023-04-19 22:43:35 -0600

    Grade P/F

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string x = Console.ReadLine();
+        int number = int.Parse(x);
+        int d = 60;
+        int c = 70;
+        int b = 80; 
+        int a = 90;
+
+        if (x < d)
+            {
+                Console.WriteLine("You have an F");
+            }
+        else if (x < c)
+            {
+                Console.WriteLine("You have an D");
+            }
+        else if (x < b)
+            {
+                Console.WriteLine("You have an c");
+            }
+        else if (x < a)
+            {
+                Console.WriteLine("You have an B");
+            }
+        else
+            {
+                Console.WriteLine("You have an A");
+            }
+        if (x < c)
+            {
+                Console.WriteLine("You didn't quite pass, but you can take this knowlege and retake the class");
+            }
+        else 
+            {
+                Console.WriteLine("You passed, Congragulations");
+            }
     }
 }
