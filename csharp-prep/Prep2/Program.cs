6e8c201056e37ae12257e3315e6fe54df135bcb3
Nathan Reynolds <rey21031@byui.edu>
2023-04-24 12:26:39 -0600

    Core Requirement 3, oops

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,35 +8,32 @@ class Program
         Console.Write("What is your grade percentage? ");
         string x = Console.ReadLine();
         int grade = int.Parse(x);
-        int d = 60;
-        int c = 70;
-        int b = 80; 
-        int a = 90;
-
-        if (grade < d)
-            {
-                Console.Write("You have an F. ");
-            }
-        else if (grade < c)
-            {
-                Console.Write("You have an D. ");
-            }
-        else if (grade< b)
-            {
-                Console.Write("You have an C. ");
-            }
-        else if (grade < a)
-            {
-                Console.Write("You have an B. ");
-            }
+        string letter = "";
+        if (grade < 60)
+        {
+            letter = "F";
+        }
+        else if (grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade < 90)
+        {
+            letter = "B";
+        }
         else
-            {
-                Console.Write("You have an A. ");
-            }
-        if (grade < c)
-            {
-                Console.Write("You didn't quite pass, but you can take this knowlege and retake the class.");
-            }
+        {
+            letter = "A";
+        }
+        Console.Write($"You have an {letter}. ");
+        if (grade < 70)
+        {
+            Console.Write("You didn't quite pass, but you can take this knowlege and retake the class.");
+        }
         else 
             {
                 Console.WriteLine("You passed, congragulations!");
